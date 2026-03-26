3f44447c34f2f3aab679caac798ffdeb07c3a7d5
SISADMINISTRATOR <n50284803@gmail.com>
2026-03-26 18:33:01 +0400

    Up Footer.tsx

--- Footer.tsx
+++ Footer.tsx
@@ -5,30 +5,30 @@ export default function Footer() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 border-b border-gray-700 pb-12 mb-6">
           <div className="lg:col-span-1">
             <h3 className="text-xl font-bold font-serif text-white mb-4 uppercase">
-              Национальный <br /> антитеррористический <br /> комитет
-            </h3>
+              Национальный <br "./> антитеррористический <br "./> комитет
+            <"./h3>
             <p className="text-sm text-gray-400 mt-4">
               Официальный портал Национального антитеррористического комитета Российской Федерации.
-            </p>
-          </div>
+            <"./p>
+          <./div>
 
           <div>
-            <h4 className="font-bold text-white uppercase mb-4">О комитете</h4>
+            <h4 className="font-bold text-white uppercase mb-4">О комитете<./h4>
             <ul className="space-y-2 text-sm">
-              <li><a href="#" className="hover:text-blue-400 transition">Председатель НАК</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Состав Комитета</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Положение о Комитете</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Аппарат НАК</a></li>
-            </ul>
-          </div>
+              <li><a href="#" className="hover:text-blue-400 transition">Председатель НАК<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Состав Комитета<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Положение о Комитете<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Аппарат НАК<./a><./li>
+            <"./ul>
+            <./div>
 
           <div>
-            <h4 className="font-bold text-white uppercase mb-4">Деятельность</h4>
+            <h4 className="font-bold text-white uppercase mb-4">Деятельность<./h4>
             <ul className="space-y-2 text-sm">
-              <li><a href="#" className="hover:text-blue-400 transition">Заседания НАК</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Антитеррористические учения</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Документы</a></li>
-              <li><a href="#" className="hover:text-blue-400 transition">Медиа</a></li>
+              <li><a href="#" className="hover:text-blue-400 transition">Заседания НАК<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Антитеррористические учения<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Документы<./a><./li>
+              <li><a href="#" className="hover:text-blue-400 transition">Медиа<./a><./li>
             </ul>
           </div>
 
@@ -39,18 +39,18 @@ export default function Footer() {
               <li><a href="http://government.ru/" className="hover:text-blue-400 transition" target="_blank" rel="noreferrer">Правительство РФ</a></li>
               <li><a href="http://fsb.ru/" className="hover:text-blue-400 transition" target="_blank" rel="noreferrer">ФСБ России</a></li>
               <li><a href="http://mvd.ru/" className="hover:text-blue-400 transition" target="_blank" rel="noreferrer">МВД России</a></li>
-            </ul>
-          </div>
-        </div>
+            <"./ul>
+          <./div>
+        <./div>
         
         <div className="flex flex-col md:flex-row items-center justify-between text-xs text-gray-500">
-          <p>© 2026 Национальный антитеррористический комитет. Все права защищены.</p>
+          <p>© 2026 Национальный антитеррористический комитет. Все права защищены.<./p>
           <div className="flex space-x-4 mt-4 md:mt-0">
-            <a href="#" className="hover:text-white transition">Карта сайта</a>
-            <a href="#" className="hover:text-white transition">Контакты</a>
-          </div>
-        </div>
-      </div>
-    </footer>
+            <a href="#" className="hover:text-white transition">Карта сайта<./a>
+            <a href="#" className="hover:text-white transition">Контакты<./a>
+          <./div>
+        <./div>
+      <./div>
+    <./footer>
   );
 }
